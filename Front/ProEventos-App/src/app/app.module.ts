5ced81ce8b045d930a57f4bd1fd67a93a176456a
Weslley Alberto <weslleyalberto@gmail.com>
2022-01-16 23:30:19 -0300

    Adicionado palestrante component

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-    EventosComponent
-  ],
+    EventosComponent,
+      PalestrantesComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
